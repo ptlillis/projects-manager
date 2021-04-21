36b7c1afd6c457be7f11b9e80758088815db511b
Ali Alaoui <madkar777@gmail.com>
2021-04-20 22:37:35 -0400

    Add react frontend

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+// serviceWorker.register();
 
 ReactDOM.render(
   <React.StrictMode>
